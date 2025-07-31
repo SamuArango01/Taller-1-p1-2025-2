91faee8279d2817380c1271e4193081907271127
Samuel Arango Echeverri <159477920+SamuArango01@users.noreply.github.com>
2025-07-31 14:50:36 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to home page</h1")
+from .models import Movie
 
-def about(request):
-    return HttpResponse("<h1>About us</h1><p>This is the about page of our movie review site.</p>")
+# Create your views here.
 
 def home(request):
     # return HttpResponse('<h1>Welcome to home page</h1>')
     # return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Samu Arango'})
+    # return render(request, 'home.html', {'name': 'Samu Arango'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+
+def about(request):
+    return HttpResponse('<h1>About Movie Reviews</h1><p>This is a project to review movies.</p>')
